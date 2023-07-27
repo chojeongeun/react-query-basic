9f98022cf695d93cb5c8cba0836d46122c61e4b3
chojeongeun <juy0816@naver.com>
2023-07-27 11:46:13 +0900

    데이터 캐싱, stale 시간 설정

--- src/UserInfo.jsx
+++ src/UserInfo.jsx
@@ -7,7 +7,12 @@ function UserInfo() {
 		return await response.json();
 	};
 
-	const { data, isLoading, isSuccess, isError } = useQuery(['user', 2], fetchUser);
+	const { data, isLoading, isSuccess, isError } = useQuery(['user', 2], fetchUser, {
+		refetchOnWindowFocus: false,
+		refetchOnMount: false,
+		staleTime: 1000 * 5, //디폴트 0ms
+		cacheTime: 1000 * 5, //캐시에 데이터 유지시켜주는 시간 디폴트 5분 해당 시간 뒤에 GC에 의해 메모리에서 값 제거
+	});
 	console.log(data);
 	console.log('isLoading', isLoading);
 	console.log('isSuccess', isSuccess);
